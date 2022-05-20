748a7ecd1ff8682297b047afaf79ee35de2b13df
Mark Armanious <markarmanus@gmail.com>
2022-05-20 18:40:17 -0230

    add rocket

--- src/Components/Pages/Landing.js
+++ src/Components/Pages/Landing.js
@@ -1,8 +1,31 @@
 import styled from "styled-components";
+import UiContext from "../../Contexts/UI";
 import Background from "../Background";
-
+import React from "react";
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+const Rocket = styled.img`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 30%);
+  bottom: 0%;
+  width: ${({ smallerDimension }) => (10 / 100) * smallerDimension}px;
+  height: auto;
+`;
 function Landing() {
-  return <Background></Background>;
+  const uiContext = React.useContext(UiContext);
+
+  return (
+    <Container>
+      <Background />
+      <Rocket
+        src={process.env.PUBLIC_URL + "/Images/Background/Rocket.png"}
+        smallerDimension={uiContext.dimensions.smaller}
+      />
+    </Container>
+  );
 }
 
 export default Landing;
